71166fa04f06cb081a80af18ad3dbe7d05a5c75b
nano <ferna.munozp@duocuc.cl>
2024-05-27 15:22:20 -0400

    Se añade el modelo de base de datos ORM

--- MuchachoStore/models.py
+++ MuchachoStore/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+# Modelo par genero
+
+class Genero(models.Model):
+    id_genero = models.AutoField(primary_key=True, db_column='idGenero')
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.genero)
+
+
+# Modelo para usuario
+
+class Usuario(models.Model):
+    rut = models.CharField(max_length=12, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido_paterno = models.CharField(max_length=50)
+    apellido_materno = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField(blank=True, null=False)
+    idGenero = models.ForeignKey(Genero, on_delete=models.CASCADE, db_column='idGenero')
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=12)
+    direccion = models.CharField(max_length=100)
+    contrasena = models.CharField(max_length=50)
+    activo = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return str(self.rut) + " " + str(self.nombre) + " " + str(self.apellido_paterno) + " " + str(self.apellido_materno) + " " + str(self.fecha_nacimiento) + " " + str(self.idGenero) + " " + str(self.correo) + " " + str(self.telefono) + " " + str(self.direccion) + " " + str(self.contrasena) + " " + str(self.activo)
