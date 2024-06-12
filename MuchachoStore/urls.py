c715908d0f41b8cf0f6f448e8c7268db3e25b457
Nano <ferna.munozp@duocuc.cl>
2024-06-12 16:12:07 -0400

    Estudien causas
    Co-authored-by: cammancilla <cammancilla@users.noreply.github.com>

--- MuchachoStore/urls.py
+++ MuchachoStore/urls.py
@@ -2,5 +2,15 @@ from Tercera_entrega.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.indexPrincipal, name='indexPrincipal'),
+    path('indexContacto/', views.indexContacto, name='indexContacto'),
+    path('audifonosec/', views.indexAudifonosec, name='audifonosec'),
+    path('especificacion/', views.indexEspecificacion, name='especificacion'),
+    path('login/', views.indexLogin, name='login'),
+    path('registro/', views.indexRegistro, name='registro'),
+    path('parlantesec/', views.indexParlantesec, name='parlantesec'),
+    path('pendrivesec/', views.indexPendrivesec, name='pendrivesec'),
+    path('servicios/', views.indexServicios, name='servicios'),
     
+
 ]
--- MuchachoStore/views.py
+++ MuchachoStore/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def indexPrincipal(request):
+    return render(request, 'indexprincipal.html')
+
+def indexContacto(request):
+    return render(request, 'indexContacto.html')
+
+def indexAudifonosec(request):
+    return render(request, 'audifonosec.html')
+
+def indexEspecificacion(request):
+    return render(request, 'especificacion.html')
+
+def indexLogin(request):
+    return render(request, 'login.html')
+
+def indexRegistro(request):
+    return render(request, 'registro.html')
+
+def indexParlantesec(request):
+    return render(request, 'parlantesec.html')
+
+def indexPendrivesec(request):
+    return render(request, 'pendrivesec.html')
+
+def indexServicios(request):
+    return render(request, 'servicios.html')
