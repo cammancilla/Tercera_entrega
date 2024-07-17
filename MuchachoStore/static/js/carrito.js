c62322ce08804bff32d97e23f08fa17002eae71a
cammancilla <cam.mancilla@duocuc.cl>
2024-07-16 23:26:29 -0400

    carrito devuelta a funcionar

--- MuchachoStore/static/js/carrito.js
+++ MuchachoStore/static/js/carrito.js
@@ -1,75 +1,61 @@
-document.addEventListener('DOMContentLoaded', loadEventListeners);
-
 let allContainerCart = document.querySelector('.products');
-let containerBuyCart = document.querySelector('.card-cart');
+let containerBuyCart = document.querySelector('.card-cart')
+
 
 let buyThings = [];
+let totalCard = 0;
+let countProduct = 0;
 
-function loadEventListeners() {
+function loadEventListenrs(){
     allContainerCart.addEventListener('click', addProduct);
     containerBuyCart.addEventListener('click', deleteProduct);
 }
 
-function addProduct(e) {
-    if (e.target.classList.contains('btn-add-cart')) {
-        const selectedProduct = e.target.closest('.product');
-        readTheContent(selectedProduct);
+function addProduct(e){
+    e.preventDefault();
+   
+    if(e.target.classList.contains('btn-add-cart')){
+    const selectProduct = e.target.parentElement.parentElement;
+    readTheContent(selectProduct);
+    console.log("Test");
     }
 }
 
-function readTheContent(product) {
+function readTheContent(product){
     const infoProduct = {
         image: product.querySelector('img').getAttribute("src"),
         title: product.querySelector('.card-title').textContent,
         price: product.querySelector('.precio').textContent,
         id: product.querySelector('button').getAttribute('data-id'),
         amount: 1
-    };
-
-    const exists = buyThings.some(product => product.id === infoProduct.id);
-    if (exists) {
-        const products = buyThings.map(product => {
-            if (product.id === infoProduct.id) {
-                product.amount++;
-                return product;
-            } else {
-                return product;
-            }
-        });
-        buyThings = [...products];
-    } else {
-        buyThings = [...buyThings, infoProduct];
-    }
+    } 
+    buyThings = [...buyThings, infoProduct]
     loadHtml();
+    console.log(infoProduct);
 }
 
-function loadHtml() {
-    clearHtml();
+function loadHtml(){
+    // clearHtml();
     buyThings.forEach(product => {
-        const { image, title, price, amount, id } = product;
+        const {image, title, price, amount, id} = product;
         const row = document.createElement('div');
         row.classList.add('card');
         row.innerHTML = `
-            <img src="${image}" alt="Product image" width="400px" height="80px">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="precio">${price}</p>
-                <h6> Amount: ${amount}</h6>
-                <span class="delete-product" data-id="${id}">X</span>
-            </div>
+        <img src="${image}" alt="cargando..." width="400px" heigth="80px">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="precio">${price}</p>
+          <h6> Amount: ${amount}</h6>
+          <span class="delete-product" data-id="${id}">X</span>
+        </div>
+
         `;
+
         containerBuyCart.appendChild(row);
     });
 }
 
-function clearHtml() {
-    containerBuyCart.innerHTML = '';
-}
+//function clearHtml(){
+    //containerBuyCart.innerHTML = '';
+//}
 
-function deleteProduct(e) {
-    if (e.target.classList.contains('delete-product')) {
-        const productId = e.target.getAttribute('data-id');
-        buyThings = buyThings.filter(product => product.id !== productId);
-        loadHtml();
-    }
-}
